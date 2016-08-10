64b5a664a4e93f5206ee98ac38431c077112cf32
Maximilian Heinrich <maximilian.heinrich@esono.de>
2016-08-10 18:12:03 +0200

    run php-fpm daemon as root but everything else as given user using gosu

--- library/php/7.0-fpm/config/patchpanel.sh
+++ library/php/7.0-fpm/config/patchpanel.sh
@@ -28,7 +28,7 @@
 #
 
 # setting run user from environment
-useradd -u $SERVICE_USER_ID $SERVICE_USER_NAME -d /home/$SERVICE_USER_NAME -s /bin/bash
+id -u $SERVICE_USER_NAME &>/dev/null | useradd -u $SERVICE_USER_ID $SERVICE_USER_NAME -d /home/$SERVICE_USER_NAME -s /bin/bash
 
 # setting run user from environment
 
@@ -53,4 +53,8 @@ if [ "true" == "$EPP_SHOW_ERRORS" ]; then
   sed -i "s/display_errors = Off/display_errors = On/g" /usr/local/etc/php/php.ini
 fi
 
-exec gosu $SERVICE_USER_ID:$SERVICE_USER_ID "$@"
+if [ "$1" = 'php-fpm' ]; then
+    exec "$@"
+else
+    exec gosu $SERVICE_USER_ID:$SERVICE_USER_ID "$@"
+fi
