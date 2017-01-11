6cf8aff36d817ec2d29cc5a80fb657fec58f1330
Maximilian Heinrich <maximilian.heinrich@esono.de>
2017-01-11 13:27:23 +0100

    add correct host name

--- library/mysql/5.6/scripts/mysql_backup.sh
+++ library/mysql/5.6/scripts/mysql_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 WEEKDAY=`/bin/date +%A`
-for i in `/usr/bin/mysql -uroot -p$MYSQL_ROOT_PASSWORD -Bs -e "show databases"`
+for i in `/usr/bin/mysql -h mysql -uroot -p$MYSQL_ROOT_PASSWORD -Bs -e "show databases"`
 do
-ionice -c3 nice -n19 mysqldump -uroot -p$MYSQL_ROOT_PASSWORD --opt --add-drop-table --quote-names --default-character-set=utf8 --single-transaction --quick --lock-tables=false "$i" | gzip > "/backup/$i.$WEEKDAY.sql.gz"
+ionice -c3 nice -n19 mysqldump -h mysql -uroot -p$MYSQL_ROOT_PASSWORD --opt --add-drop-table --quote-names --default-character-set=utf8 --single-transaction --quick --lock-tables=false "$i" | gzip > "/backup/$i.$WEEKDAY.sql.gz"
 done
